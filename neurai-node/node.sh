ca79adad346f56ab191a3a5c8bb5385417dad90c
NeuraiProject <122438095+NeuraiProject@users.noreply.github.com>
2025-12-10 10:52:09 +0100

    Modify Neurai node configuration settings

--- neurai-node/node.sh
+++ neurai-node/node.sh
@@ -12,23 +12,26 @@ touch /data/node/neurai.conf
 cat <<- EOF > /data/node/neurai.conf
 rpcuser=${RPC_USER:-NeuraiDockerUser}
 rpcpassword=${RPC_PASS:-NeuraiDockerPassword}
-listen=1
+listen=0
 daemon=0
 server=1
-rest=1
-dbcache=16
-rpcworkqueue=1024
-rpcthreads=64
-rpcallowip=0.0.0.0/0
-disablewallet=1
-rpcbind=0.0.0.0
 txindex=1
 assetindex=1
 addressindex=1
 timestampindex=1
+spentindex=1
+pubkeyindex=1
+rest=1
+dbcache=16
+rpcport=${RPC_PORT:-19001}
+port=${PORT:-19000}
+rpcworkqueue=1024
+rpcthreads=16
+rpcallowip=0.0.0.0/0
+disablewallet=1
+rpcbind=0.0.0.0
 rpcservertimeout=240
 EOF
 fi
 
-echo -e "Starting Neurai node daemon..."
 bash -c "neuraid -datadir=/data/node"
